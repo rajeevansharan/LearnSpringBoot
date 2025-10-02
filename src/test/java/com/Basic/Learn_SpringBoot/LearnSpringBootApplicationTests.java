57a923088a82b9c0cd17a1493decea3963398a3b
rajeevansharan <rajeevansharan@gmail.com>
2025-10-02 14:45:18 +0530

    first commit

---
+++ src/test/java/com/Basic/Learn_SpringBoot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Basic.Learn_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
